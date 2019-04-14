f1920d965a542cfea39c7b34a874ca159f2266da
wjuniori <wjuniori.si@gmail.com>
2019-04-13 22:58:46 -0300

    feat: Build a single-page web app that allows users to draw pixel art on a customizable canvas
    JavaScript code lets the user create a grid of squares representing their design, and apply colors to those squares to create a digital masterpiece.
    
    Your users should be able to:
    
    - Dynamically set the size of the table as an N by M grid.
    - Choose a color.
    - Click a cell in the grid to fill that cell with the chosen color.
    
    Primary task went to implement the makeGrid() function, that dynamically creates a grid that the user can interact with.

--- designs.js
+++ designs.js
@@ -1,10 +1,74 @@
-// Select color input
-// Select size input
+'use strict'; // Start of use strict
 
-// When size is submitted by the user, call makeGrid()
+// Hides table, removes its grid (its rows) and displays table
+function destroyGrid(grid) {
 
-function makeGrid() {
+  const trs = grid.querySelectorAll('tr');
 
-// Your code goes here!
+  if (trs.length) {
+
+    grid.setAttribute('style', 'display: none;');
+
+    for (let i = 0; i < trs.length; i++) {
+      trs[i].remove();
+    }
+
+    grid.removeAttribute('style');
+  }
 
 }
+
+// Sets table size as a grid N (height) by M (width)
+function makeGrid(grid, height, width) {
+
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < height; i++) {
+
+    const tr = document.createElement('tr');
+
+    for (let j = 0; j < width; j++) {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+    }
+
+    fragment.appendChild(tr);
+
+  }
+
+  grid.appendChild(fragment);
+
+}
+
+// Executes when DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Creates an empty grid N (height) by M (width) when form is submited
+  document.querySelector('#sizePicker').addEventListener('submit', function(event) {
+
+    const pixelCanvas = document.getElementById('pixelCanvas');
+    const inputHeight = document.getElementById('inputHeight');
+    const inputWidth = document.getElementById('inputWidth');
+
+    // Prevents event default action
+    event.preventDefault();
+
+    destroyGrid(pixelCanvas);
+    makeGrid(pixelCanvas, inputHeight.valueAsNumber, inputWidth.valueAsNumber);
+
+  });
+
+  // Changes color of a grid square (and only that square) when that square (a td element) is clicked
+  document.querySelector('#pixelCanvas').addEventListener('click', function(event) {
+
+    // Delegates the click event to td element
+    if (event.target.nodeName.toLowerCase() === 'td') {
+      const color = document.getElementById('colorPicker');
+      event.target.style.backgroundColor = color.value;
+    }
+
+  });
+
+});
+
+// End of use strict
